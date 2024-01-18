33da3674c7676e82ab8df425b4d214303e7049b5
Yuseidosukoiya <y.fukushima.23.5.964@4nonome.com>
2024-01-18 16:29:08 +0900

    Atoms/Inputの作成

---
+++ src/components/Atoms/Input/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+
+import COLOR from "../../../variables/color";
+import TEXT from "../../../variables/texts";
+
+export const Input = () => {
+  const inputRef = useRef(null);
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const onEditComplete = {};
+
+  const handleBlur = () => {
+    return onEditComplete;
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      return onEditComplete;
+    }
+  };
+
+  return (
+    <StyledInput
+      ref={inputRef}
+      onblur={handleBlur}
+      onKeyDown={handleKeyDown}
+      defaultValue=""
+    />
+  );
+};
+
+const StyledInput = styled.input`
+  width: 100%;
+  border: none;
+  outline: none;
+  border-radius: 2px;
+  padding: 0px 4px;
+  ${TEXT.S}
+  color: ${COLOR.LIGHT_GRAY};
+  background-color: ${COLOR.BLACK};
+`;
