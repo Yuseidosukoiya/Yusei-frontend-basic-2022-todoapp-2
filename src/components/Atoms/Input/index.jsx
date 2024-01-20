ac82152aa21a9607b2843d119d4763b2bc2f2528
Yuseidosukoiya <y.fukushima.23.5.964@4nonome.com>
2024-01-20 11:41:19 +0900

    defaultValueに初期値" "を渡す

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import COLOR from "../../../variables/color";
 import TEXT from "../../../variables/texts";
 
-export const Input = ({ onEditComplete, defaultValue }) => {
+export const Input = ({ onEditComplete, defaultValue = "" }) => {
   const inputRef = useRef(null);
   useEffect(() => {
     inputRef.current.focus();
